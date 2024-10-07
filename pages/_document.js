56cdd0c83dc652c7cc0ba61b3ebf9ad4d3058668
Samaras7 <patryk.piworowicz@gmail.com>
2024-10-07 17:25:08 +0200

    ReactProject

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+          />
+          <link rel="stylesheet" href="./css/globals.css" />
+        </Head>
+        <body>
+          <div className="container">
+            <Main />
+          </div>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import App from "../app/components/app";
+
+const Index = () => <App />; 
+
+export default Index;
